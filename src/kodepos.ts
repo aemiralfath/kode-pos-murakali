c6566f96979024e2c033497357cd4ba32ff63f28
Ahmad Emir Alfatah <ahmad.alfatah@seamoney.com>
2022-12-21 23:40:24 +0700

    change to http://nomorkodepos.com/

--- src/kodepos.ts
+++ src/kodepos.ts
@@ -1,89 +1,95 @@
-import HeaderGenerator from 'header-generator'
-import cheerio from 'cheerio'
-import axios from 'axios'
-import { DataResponse, DataResult, DataResults } from './types'
+import HeaderGenerator from "header-generator";
+import cheerio from "cheerio";
+import axios from "axios";
+import { DataResponse, DataResult, DataResults } from "./types";
 
 class Kodepos {
-	private readonly baseurl: string = 'https://nomorkodepos.com/'
-	private readonly keywords: string
-	private readonly headers: object
+  private readonly baseurl: string = "http://nomorkodepos.com/";
+  private readonly keywords: string;
+  private readonly headers: object;
 
-	constructor(keywords: string) {
-		this.keywords = keywords
+  constructor(keywords: string) {
+    this.keywords = keywords;
 
-		this.headers = new HeaderGenerator({
-			browsers: ['chrome', 'firefox', 'safari'],
-			operatingSystems: ['linux', 'android', 'windows'],
-			devices: ['desktop', 'mobile'],
-			locales: ['id-ID']
-		})
-	}
+    this.headers = new HeaderGenerator({
+      browsers: ["chrome", "firefox", "safari"],
+      operatingSystems: ["linux", "android", "windows"],
+      devices: ["desktop", "mobile"],
+      locales: ["id-ID"],
+    });
+  }
 
-	public async search(): Promise<DataResponse> {
-		const url = this.baseurl + '?s=' + this.keywords
+  public async search(): Promise<DataResponse> {
+    const url = this.baseurl + "?s=" + this.keywords;
 
-		try {
-			let output = await axios({
-				method: 'GET',
-				url,
-				headers: this.headers
-			})
-			const $: cheerio.Root = cheerio.load(output.data)
+    try {
+      let output = await axios({
+        method: "GET",
+        url,
+        headers: this.headers,
+      });
+      const $: cheerio.Root = cheerio.load(output.data);
 
-			let tr: cheerio.Cheerio = $('tr')
-			if (tr.length > 0) {
-				let results: DataResults = []
+      let tr: cheerio.Cheerio = $("tr");
+      if (tr.length > 0) {
+        let results: DataResults = [];
 
-				tr.each((number: number, element: cheerio.Element): void => {
-					if (number === 0) return
+        tr.each((number: number, element: cheerio.Element): void => {
+          if (number === 0) return;
 
-					let td: cheerio.Cheerio = $(element).find('td')
-					let result: DataResult = {}
+          let td: cheerio.Cheerio = $(element).find("td");
+          let result: DataResult = {};
 
-					td.each((index: number, html: cheerio.Element): void => {
-						let value: string = $(html).find('a').text()
-						let key: string = index === 0 ? 'province' :
-							(index === 1 ? 'city' :
-								(index === 2 ? 'subdistrict' :
-									(index === 3 ? 'urban' : 'postalcode')))
+          td.each((index: number, html: cheerio.Element): void => {
+            let value: string = $(html).find("a").text();
+            let key: string =
+              index === 0
+                ? "province"
+                : index === 1
+                ? "city"
+                : index === 2
+                ? "subdistrict"
+                : index === 3
+                ? "urban"
+                : "postalcode";
 
-						result[key] = value.trim()
-					})
+            result[key] = value.trim();
+          });
 
-					if (Object.entries(result).length === 5) {
-						results.push(result)
-					}
-				})
+          if (Object.entries(result).length === 5) {
+            results.push(result);
+          }
+        });
 
-				let response: DataResponse = {
-					code: 200,
-					status: true,
-					messages: 'Data search successfully parsed.',
-					data: results
-				}
+        let response: DataResponse = {
+          code: 200,
+          status: true,
+          messages: "Data search successfully parsed.",
+          data: results,
+        };
 
-				return response
-			} else {
-				let response: DataResponse = {
-					code: 200,
-					status: false,
-					messages: 'No data can be returned.'
-				}
+        return response;
+      } else {
+        let response: DataResponse = {
+          code: 200,
+          status: false,
+          messages: "No data can be returned.",
+        };
 
-				return response
-			}
-		} catch (error) {
-			console.error(error)
+        return response;
+      }
+    } catch (error) {
+      console.error(error);
 
-			let response: DataResponse = {
-				code: 500,
-				status: false,
-				messages: 'An error occurred in the script.'
-			}
+      let response: DataResponse = {
+        code: 500,
+        status: false,
+        messages: "An error occurred in the script.",
+      };
 
-			return response
-		}
-	}
+      return response;
+    }
+  }
 }
 
-export default Kodepos
+export default Kodepos;
