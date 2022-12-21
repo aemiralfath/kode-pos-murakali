449b07c08894734568e1eefb5462998bcf7f5f41
Ahmad Emir Alfatah <ahmad.alfatah@seamoney.com>
2022-12-22 01:37:41 +0700

    add multiple pages

--- src/kodepos.ts
+++ src/kodepos.ts
@@ -1,89 +1,131 @@
-import HeaderGenerator from 'header-generator'
-import cheerio from 'cheerio'
-import axios from 'axios'
-import { DataResponse, DataResult, DataResults } from './types'
+import HeaderGenerator from "header-generator";
+import cheerio from "cheerio";
+import axios from "axios";
+import { DataResponse, DataResult, DataResults } from "./types";
+import { val } from "cheerio/lib/api/attributes";
 
 class Kodepos {
-	private readonly baseurl: string = 'https://nomorkodepos.com/'
-	private readonly keywords: string
-	private readonly headers: object
+  private readonly baseurl: string = "https://carikodepos.com/";
+  private readonly keywords: string;
+  private readonly headers: object;
 
-	constructor(keywords: string) {
-		this.keywords = keywords
+  constructor(keywords: string) {
+    this.keywords = keywords;
 
-		this.headers = new HeaderGenerator({
-			browsers: ['chrome', 'firefox', 'safari'],
-			operatingSystems: ['linux', 'android', 'windows'],
-			devices: ['desktop', 'mobile'],
-			locales: ['id-ID']
-		})
-	}
+    this.headers = new HeaderGenerator({
+      browsers: ["chrome", "firefox", "safari"],
+      operatingSystems: ["linux", "android", "windows"],
+      devices: ["desktop", "mobile"],
+      locales: ["id-ID"],
+    });
+  }
 
-	public async search(): Promise<DataResponse> {
-		const url = this.baseurl + '?s=' + this.keywords
+  public async search(): Promise<DataResponse> {
+    let url = this.baseurl + "/page/1/?s=" + this.keywords;
+    let last = 1;
+    try {
+      let output = await axios({
+        method: "GET",
+        url,
+        headers: this.headers,
+      });
+      const $: cheerio.Root = cheerio.load(output.data);
+      let lastLink: string | undefined = $(".last").first().attr("href");
+      last = parseInt(lastLink?.substring(29, 31)!);
+    } catch (error) {
+      console.error(error);
+    }
 
-		try {
-			let output = await axios({
-				method: 'GET',
-				url,
-				headers: this.headers
-			})
-			const $: cheerio.Root = cheerio.load(output.data)
+    try {
+      let indexArr: number[] = [];
+      for (let index = 1; index <= last; index++) {
+        indexArr.push(index);
+      }
 
-			let tr: cheerio.Cheerio = $('tr')
-			if (tr.length > 0) {
-				let results: DataResults = []
+      let results: DataResults = [];
+      const promises = indexArr.map(async (value) => {
+        let res = await this.searchIndex(value);
+        return res;
+      });
 
-				tr.each((number: number, element: cheerio.Element): void => {
-					if (number === 0) return
+      const numRes = await Promise.all(promises);
+      numRes.forEach((res) => {
+        res.forEach((value) => {
+          results.push(value);
+        });
+      });
 
-					let td: cheerio.Cheerio = $(element).find('td')
-					let result: DataResult = {}
+      let response: DataResponse = {
+        code: 200,
+        status: true,
+        messages: "Data search successfully parsed.",
+        data: results,
+      };
 
-					td.each((index: number, html: cheerio.Element): void => {
-						let value: string = $(html).find('a').text()
-						let key: string = index === 0 ? 'province' :
-							(index === 1 ? 'city' :
-								(index === 2 ? 'subdistrict' :
-									(index === 3 ? 'urban' : 'postalcode')))
+      return response;
+    } catch (error) {
+      console.error(error);
 
-						result[key] = value.trim()
-					})
+      let response: DataResponse = {
+        code: 500,
+        status: false,
+        messages: "An error occurred in the script.",
+      };
 
-					if (Object.entries(result).length === 5) {
-						results.push(result)
-					}
-				})
+      return response;
+    }
+  }
 
-				let response: DataResponse = {
-					code: 200,
-					status: true,
-					messages: 'Data search successfully parsed.',
-					data: results
-				}
+  public async searchIndex(index: number): Promise<DataResults> {
+    const url = this.baseurl + "/page/" + index + "/?s=" + this.keywords;
 
-				return response
-			} else {
-				let response: DataResponse = {
-					code: 200,
-					status: false,
-					messages: 'No data can be returned.'
-				}
+    let results: DataResults = [];
+    try {
+      let output = await axios({
+        method: "GET",
+        url,
+        headers: this.headers,
+      });
+      const $: cheerio.Root = cheerio.load(output.data);
 
-				return response
-			}
-		} catch (error) {
-			console.error(error)
+      let result: DataResult = {};
+      let tr: cheerio.Cheerio = $("tr");
+      if (tr.length > 0) {
+        tr.each((number: number, element: cheerio.Element): void => {
+          if (number === 0) return;
 
-			let response: DataResponse = {
-				code: 500,
-				status: false,
-				messages: 'An error occurred in the script.'
-			}
+          let td: cheerio.Cheerio = $(element).find("td");
 
-			return response
-		}
-	}
+          td.each((index: number, html: cheerio.Element): void => {
+            let value: string = $(html).find("a").text();
+            let key: string =
+              index === 0
+                ? "province"
+                : index === 1
+                ? "city"
+                : index === 2
+                ? "subdistrict"
+                : index === 3
+                ? "urban"
+                : "postalcode";
+
+            result[key] = value.trim();
+          });
+
+          if (Object.entries(result).length === 5) {
+            results.push(result);
+          }
+        });
+
+        return results;
+      } else {
+        return results;
+      }
+    } catch (error) {
+      console.error(error);
+      return results;
+    }
+  }
 }
 
-export default Kodepos
+export default Kodepos;
